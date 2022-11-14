542d022707f4f8d8f2a3f0fe957120a0167348b2
Ivan Kovalenko <gunonu@yandex.ru>
2022-11-14 16:25:42 +0300

    second

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const mainBlk = document.querySelector('.main-block');
 const image = mainBlk.querySelector('.picture');
 const btn = mainBlk.querySelector('.button-change-img');
+const spinner = mainBlk.querySelector('.spinner');
 
-fetch('https://api.unsplash.com/photos/random/?client_id=rNbUjGL5BpnOl_37-24eWw-dmjuP7UVRKiki11xcF_k')
-.then(res => res.json())
-.then((data) => {
-  console.log(data);
-});
+// fetch('https://api.unsplash.com/photos/random/?client_id=rNbUjGL5BpnOl_37-24eWw-dmjuP7UVRKiki11xcF_k')
+// .then(res => res.json())
+// .then((data) => {
+//   console.log(data);
+// });
 
 
 
@@ -25,14 +26,45 @@ fetch('https://api.unsplash.com/photos/random/?client_id=rNbUjGL5BpnOl_37-24eWw-
 //   }
 //   return Promise.reject(`Ошибка: ${res.status}`);
 // };
+// https://swapi.nomoreparties.co/planets/1
 
 
 
 
 btn.addEventListener('click', () => {
-  image.setAttribute('src', 'https://images.unsplash.com/photo-1667816014414-dba47e785cc1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80');
+  renderLoading(true);
+  fetch('https://api.unsplash.com/photos/random/?client_id=rNbUjGL5BpnOl_37-24eWw-dmjuP7UVRKiki11xcF_k')
+  .then((res) => {
+      if (res.ok) {
+      return res.json();
+    }
+      return Promise.reject(res.status);
+  })
+  // .then((res) => {
+  //   console.log(res);
+  // })
+  .then((res) => {
+    renderResult(res.urls.regular);
+  })
+  .catch((err) => {
+    console.log(`Ошибка: ${err}`);  
+  })
+  .finally(() => {
+    renderLoading(false);
+  });
 });
 
+function renderResult(url) {
+  image.setAttribute('src', `${url}`);
+}
+
+function renderLoading(isLoading) {
+  if (isLoading) {
+    spinner.classList.add('spinner_visible');
+  } else {
+    spinner.classList.remove('spinner_visible');
+  }
+}
 
 
 
